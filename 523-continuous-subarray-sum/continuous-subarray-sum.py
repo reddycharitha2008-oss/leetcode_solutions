1e909c73e68da9b7313f71747ef0b64c213aa051
Charitha_Reddy <reddycharitha2008@gmail.com>
2026-09-10 10:34:49 +0530

    Time: 50 ms (72.74%) | Memory: 39.6 MB (24.03%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -3,14 +3,14 @@ class Solution:
         prefix = 0
         mp = {0: -1}
 
-        for i in range(len(nums)):
-            prefix += nums[i]
+        for i,num in enumerate(nums):
+            prefix += num
             remainder = prefix % k
 
             if remainder in mp:
                 if i - mp[remainder] >= 2:
                     return True
             else:
-                mp[remainder] = i
+                mp[remainder]=i
 
         return False
